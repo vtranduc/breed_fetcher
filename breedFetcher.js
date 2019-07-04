4ca6fcaae29e9ec68c8fe194ca3afede56a82ca0
Viet Nhien Tran Duc <vtranduc@ryerson.ca>
2019-07-04 17:11:28 +0000

    updated

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const breedName = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+request(url, (error, resp, body) => {
+  // console.log(body);
+  // console.log(typeof body);
+
+  if (error) {
+    return console.log('Failed to request details: ', error);
+  }
+
+  const data = JSON.parse(body);
+  //console.log(data);
+  //console.log(data.length)
+
+  const breed = data[0];
+  if (breed) {
+    console.log(breed.description);
+  } else {
+    console.log(`Failed to find breed ${breedName}`);
+  }
+});
